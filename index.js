a7e567efe022ea441a2490b65de28ad4f706b30b
Boris <risentveber@gmail.com>
2017-03-08 16:29:20 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+path = require('path');
+
+function metalsmithInspectFiles() {
+    return function printer(files, metalsmith, done) {
+        const splited = Object.keys(files).sort().map(name => name.split(path.sep));
+        const createNode = (name, path) => ({
+            name,
+            path,
+            nodes: [],
+            lists: []
+        });
+        const root = createNode('', '');
+        let current = root;
+
+        splited.forEach(path => {
+            const len = path.length;
+
+            for(let i = 0; i < len; i++) {
+                const isList = i + 1 === len;
+                const nextPath = current.path + path.sep + path[i];
+                let nextNode = current.nodes.find(child => child.path === nextPath);
+
+                if (!nextNode) {
+                    nextNode = createNode(path[i], nextPath);
+                    if (isList) {
+                        current.lists.push(nextNode);
+                    } else {
+                        current.nodes.push(nextNode);
+                    }
+                }
+
+                current = nextNode;
+            }
+        });
+
+        function print(node, prefix, parentIsLast){
+            process.stdout.write(prefix.join('') + node.name + '\n');
+            const children = node.nodes.concat(node.lists);
+
+            if (children.length) {
+                const len = children.length;
+                for(let i = 0; i < len; i++) {
+                    const isLast = i + 1 === len;
+                    const nextPrefix = prefix.slice();
+                    if (parentIsLast) {
+                        nextPrefix.pop();
+                        nextPrefix.push('    ');
+                    }
+
+                    nextPrefix.push(isLast ? '   -' : '  |-');
+
+                    print(children[i], nextPrefix, isLast);
+                }
+            }
+        }
+
+        process.stdout.write('\x1b[1m');
+        print(root, [''], true);
+        process.stdout.write('\x1b[0m');
+
+        done();
+    }
+}
+
+module.exports = metalsmithInspectFiles;
---
+++ readme.md
@@ -0,0 +1,20 @@
+[![NPM](https://nodei.co/npm/metalsmith-inspect-files.png)](https://npmjs.org/package/metalsmith-inspect-files)
+## metalsmith inspect files
+
+This is metalsmith plugin that prints files tree in selected build step.
+
+Example with printing build result:
+```ecmascript 6
+const Metalsmith = require('metalsmith');
+const metalsmithInspectFiles = require('metalsmith-inspect-files');
+
+Metalsmith(__dirname)
+    .source('./source')
+    .destination('./build')
+    .clean(true)
+    .use(metalsmithInspectFiles())
+    .build((err, files) => {
+        if (err) { throw err; }
+    });
+
+```
